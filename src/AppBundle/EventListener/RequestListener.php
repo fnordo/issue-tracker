ff02f62f4e94a7f6b7568760b62a6ba0f3ffd138
fnordo <gassnerw@gmail.com>
2017-02-24 12:45:55 +0100

    added description to homepage, redirect already logged in users

---
+++ src/AppBundle/EventListener/RequestListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * RequestListener.
+ *
+ * @author Wolfgang Gassner <gassnerw@gmail.com>
+ */
+class RequestListener
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authorizationChecker;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(TokenStorageInterface $tokenStorage,AuthorizationCheckerInterface $authorizationChecker,
+                                RouterInterface $router)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->router = $router;
+    }
+
+    /**
+     * If an authenticated user visits the login page, redirect to homepage.
+     *
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest() || !$this->tokenStorage->getToken()) {
+            return;
+        }
+
+        if (false === $this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return;
+        }
+
+        if ($event->getRequest()->get('_route') === 'fos_user_security_login') {
+            $url = $this->router->generate('issue_index');
+            $event->setResponse(new RedirectResponse($url));
+        }
+    }
+} 
